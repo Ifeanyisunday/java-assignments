bcb02c78ecd0165c2799ce0110f2a97602e7b61b
Ifeanyisunday <165680424+Ifeanyisunday@users.noreply.github.com>
2024-04-02 08:55:46 +0100

    Sum of Digits
    java assignment

---
+++ Table.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Table{
+	public static void main(String... args){
+
+	Scanner input = new Scanner(System.in);
+
+	System.out.printf("%5s%8s%16s", "a", "b", "pow(a, b)\n");
+	
+	int a = 1;
+	int b = 2;
+	
+	int counter = 1;
+	while(counter <= 5){
+	counter ++;
+	double power = Math.pow(a, b);
+
+	
+	System.out.printf("%5d%8d      %-10.0f%n", a, b, power);
+	a++;
+	b++;
+	
+
+	}
+        
+}	
+}
