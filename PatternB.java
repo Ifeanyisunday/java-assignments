94f5b2bb08942267596e45725005b2c9d56db5ca
Ifeanyisunday <165680424+Ifeanyisunday@users.noreply.github.com>
2024-04-02 08:50:18 +0100

    Triangle Pattern B
    java assignment

---
+++ PatternB.java
@@ -0,0 +1,23 @@
+public class PatternB {
+	public static void main(String[] args){
+
+	System.out.println("Pattern B\n");
+
+	for(int count = 6; count <= 6; count--){
+		for(int counter = 1; counter <= count; counter++){
+			System.out.printf("%2d", counter);
+
+	}
+	System.out.println("");
+	if (count == 0){
+		break;
+
+	}
+	}
+
+
+}
+
+
+
+}
