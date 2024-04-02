8eec7409dc109818244b6499faf714a579efb57f
Ifeanyisunday <165680424+Ifeanyisunday@users.noreply.github.com>
2024-04-02 08:51:54 +0100

    power
    java assignment

---
+++ Power.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Power{
+	public static void main(String... args){
+
+	Scanner input = new Scanner(System.in);
+
+	System.out.println("Enter first number: ");
+	int number1 = input.nextInt();
+	
+	System.out.println("Enter second number: ");
+	int number2 = input.nextInt();
+
+	int raisePower = 1;
+
+	int counter = 1;
+	while(counter <= number2){
+	counter++;
+	raisePower*= number1;
+	}
+
+        System.out.println(number1 + " raise to power " + number2 + " is = " + raisePower);
+
+	
+}	
+}
